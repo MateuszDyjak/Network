b7aef92938ca8d5be76613cbfc5241967235cc70
MateuszDyjak <mateuszdyjak5@gmail.com>
2017-03-26 18:55:45 +0200

    new plugin added

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,37 +1,15 @@
-function init() {
-	document.addEventListener("deviceready",onDeviceReady, false);
-}
+function checkConnection() {
+    var networkState = navigator.connection.type;
 
-function onDeviceReady() {
-	navigator.notification.beep(1);
-}
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
 
-function deviceInfo() {
-
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' +
-			'Device Cordova : '  + device.cordova + '\n' +
-			'Device Platform: ' + device.platform + '\n' +
-			'Device UUID    : '     + device.uuid     + '\n' +
-			'Device Model   : '    + device.model     + '\n' +
-			'Device Version : '  + device.version  + '\n';
-
-
-	navigator.notification.alert(info);
-
-}
-function displayMyData() {
-
-	info= 'Hello! Below you can find some my personal data \n'
-
-	+ 'Name : Mateusz\n'
-	+ 'Last name : Dyjak\n'
-	+ 'City : Kraków\n'
-	+ 'Settlement : Dąbie\n'
-	+ 'District : Grzegórzki'
-	+ 'email address : mateuszdyjak5@gmail.com' ;
-
-navigator.notification.alert(info);
-
+    alert('Connection type: ' + states[networkState]);
 }
